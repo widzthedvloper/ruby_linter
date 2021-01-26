916b03a96049b7c5ab7c6bc7b685794ee821435f
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-01-25 23:47:57 -0400

    add code to rescue error an inform user when path is incorrect

--- lib/read_file.rb
+++ lib/read_file.rb
@@ -1,9 +1,17 @@
+# rubocop:disable Style/RescueStandardError
+# rubocop:disable Style/RedundantBegin
 class ReadFile
   attr_reader :file_path, :lines, :line_number
 
   def initialize(file_path)
-    @file_path = file_path
-    @lines = File.readlines(@file_path)
-    @line_number = @lines.size
+    begin
+      @file_path = file_path
+      @lines = File.readlines(@file_path)
+      @line_number = @lines.size
+    rescue => e
+      puts "There's a problem with the path you provide #{e}"
+    end
   end
 end
+# rubocop:enable Style/RescueStandardError
+# rubocop:enable Style/RedundantBegin
