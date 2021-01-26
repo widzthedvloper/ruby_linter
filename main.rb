a41bcd055468f013223893e3e5efd0a9b25baf84
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-01-25 23:33:38 -0400

    add code to display message when there's no offenses in a file

--- main.rb
+++ main.rb
@@ -7,5 +7,9 @@ msg.check_camel_case
 msg.space_btw_methods
 msg.line_length
 msg.space_after_line
-puts msg.errors
-puts msg.line_number
+if !msg.errors.empty?
+  puts msg.errors
+  puts "This file contains #{msg.line_number} lines of code"
+else
+  puts "there's no offenses in this file"
+end
