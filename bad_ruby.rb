9909f1dbf31c9791d0c20eccef45d26a78397edd
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-01-26 12:48:08 -0400

    fixing linter

--- bad_ruby.rb
+++ bad_ruby.rb
@@ -1,20 +1,22 @@
-# rubocop:disable Lint/Syntax
-class justForCheck1
-  def initialize(num)
-    @num = num  
-  end
-end
+# rubocop:disable Layout/CommentIndentation
+# rubocop:disable Layout/LineLength
+# class justForCheck1
+  # def initialize(num)
+    # @num = num
+  # end
+# end
 
-class justForCheck2
-  def initialize(num)
-    @num = num jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-  end
-end
+# class justForCheck2
+  # def initialize(num)
+    # @num = num jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+  # end
+# end
 
-class JustForCheck3
+# class JustForCheck3
 
-  def initialize(num)
-    @num = num
-  end
-end
-# rubocop:enable Lint/Syntax
+  # def initialize(num)
+    # @num = num
+  # end
+# end
+# rubocop:enable Layout/CommentIndentation
+# rubocop:enable Layout/LineLength
