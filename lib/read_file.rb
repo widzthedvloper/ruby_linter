69a5860fb613410f2659fdbe86fe079f3bc4792a
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-01-26 18:46:43 -0400

    Remove redundant begin keyword in read_file

--- lib/read_file.rb
+++ lib/read_file.rb
@@ -1,16 +1,12 @@
-# rubocop:disable Style/RedundantBegin
 class ReadFile
   attr_reader :file_path, :lines, :line_number
 
   def initialize(file_path)
-    begin
-      @file_path = file_path
-      @lines = File.readlines(@file_path)
-      @line_number = @lines.size
-    rescue StandardError => e
-      @lines = []
-      puts "There's a problem with the path you provide #{e}"
-    end
+    @file_path = file_path
+    @lines = File.readlines(@file_path)
+    @line_number = @lines.size
+  rescue StandardError => e
+    @lines = []
+    puts "There's a problem with the path you provide #{e}"
   end
 end
-# rubocop:enable Style/RedundantBegin
