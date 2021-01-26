2b121a241546c48f9618ac26dca892e89b0f3ff7
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-01-26 18:42:53 -0400

    Uncomment bad_ruby file

--- bad_ruby.rb
+++ bad_ruby.rb
@@ -1,22 +1,22 @@
 # rubocop:disable Layout/CommentIndentation
 # rubocop:disable Layout/LineLength
-# class justForCheck1
-  # def initialize(num)
-    # @num = num
-  # end
-# end
+class justForCheck1
+  def initialize(num)
+    @num = num
+  end
+end
 
-# class justForCheck2
-  # def initialize(num)
-    # @num = num jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-  # end
-# end
+class justForCheck2
+  def initialize(num)
+    @num = num jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+  end
+end
 
-# class JustForCheck3
+class JustForCheck3
 
-  # def initialize(num)
-    # @num = num
-  # end
-# end
+  def initialize(num)
+    @num = num
+  end
+end
 # rubocop:enable Layout/CommentIndentation
 # rubocop:enable Layout/LineLength
